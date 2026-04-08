724280dd102900bc4afd96b46c11c47116da6166
ziadAlattar <ziadalattar22@gmail.com>
2026-04-08 04:50:06 +0200

    tryFix

--- DB/connection.js
+++ DB/connection.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
 
-const connection = () => {
-  // mongoose.set("bufferCommands", false);
+// const connection = () => {
+//   // mongoose.set("bufferCommands", false);
 
-  return mongoose.connect(process.env.MONGO_URI, {
-    serverSelectionTimeoutMS: 30001,
-  });
-};
-
+//   return mongoose.connect(process.env.MONGO_URI, {
+//     serverSelectionTimeoutMS: 30000,
+//   });
+// };
+const connection = async ()=>{
+    return await mongoose.connect(process.env.MONGO_URI)
+    .then(()=> console.log(`connected on ...... `))
+    .catch(err=>console.log(`fail to connect `))
+}
 export default connection;
